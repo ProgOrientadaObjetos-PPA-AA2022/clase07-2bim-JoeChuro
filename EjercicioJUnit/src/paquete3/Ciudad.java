dc4a31699ef3cb9ae996bb31246e04f8cabd7a25
JoeChuro <joechuro1206@gmail.com>
2022-07-22 16:33:59 -0500

    22 DE JULIO

--- EjercicioJUnit/src/paquete3/Ciudad.java
+++ EjercicioJUnit/src/paquete3/Ciudad.java
@@ -10,6 +10,30 @@ package paquete3;
  * @author reroes
  */
 public class Ciudad {
-    
-    
+
+    private String nombre;
+    private int poblacion;
+
+    public void establecerNombres(String n) {
+        nombre = n;
+    }
+
+    public String obtenerNombres() {
+        return nombre;
+    }
+
+    public void establecerPoblacion(int p) {
+        poblacion = p;
+    }
+
+    public int obtenerPoblacion() {
+        return poblacion;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("Ciudad de Ecuador\n Nombre: %s\n\n\t"
+                + "Población: %d\n", nombre, poblacion);
+        return cadena;
+    }
 }
---
+++ EjercicioJUnit/test/paquete1/EstudiantePresencialTest.java
@@ -0,0 +1,138 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package paquete1;
+
+import java.lang.reflect.Field;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author SALA I
+ */
+public class EstudiantePresencialTest {
+
+    private EstudiantePresencial instance;
+
+    public EstudiantePresencialTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        instance = new EstudiantePresencial();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of establecerNumeroCreditos method, of class EstudiantePresencial.
+     */
+    @Test
+    public void testEstablecerNumeroCreditos() throws
+            IllegalArgumentException,
+            NoSuchFieldException,
+            IllegalAccessException {
+        System.out.println("establecerNumeroCreditos");
+        int numero = 5;
+        instance.establecerNumeroCreditos(numero);
+        Field field = instance.getClass().getDeclaredField("numeroCreditos");
+        field.setAccessible(true);
+        assertEquals(field.get(instance), 5);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of establecerCostoCredito method, of class EstudiantePresencial.
+     */
+    @Test
+    public void testEstablecerCostoCredito() throws
+            IllegalArgumentException,
+            NoSuchFieldException,
+            IllegalAccessException {
+        System.out.println("establecerCostoCredito");
+        double valor = 1.0;
+        instance.establecerCostoCredito(valor);
+        Field field = instance.getClass().getDeclaredField("costoCredito");
+        field.setAccessible(true);
+        assertEquals(field.get(instance), 1.0);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcularMatriculaPresencial method, of class
+     * EstudiantePresencial.
+     */
+    @Test
+    public void testCalcularMatriculaPresencial() throws
+            IllegalArgumentException,
+            NoSuchFieldException,
+            IllegalAccessException {
+        System.out.println("calcularMatriculaPresencial");
+        instance.establecerCostoCredito(1.0);
+        instance.establecerNumeroCreditos(5);
+        instance.calcularMatriculaPresencial();
+        Field field = instance.getClass().getDeclaredField("matriculaPresencial");
+        field.setAccessible(true);
+        assertEquals(field.get(instance), 5.0);
+    }
+
+    /**
+     * Test of obtenerNumeroCreditos method, of class EstudiantePresencial.
+     */
+    @Test
+    public void testObtenerNumeroCreditos() {
+        System.out.println("obtenerNumeroCreditos");
+        int expResult = 5;
+        instance.establecerNumeroCreditos(5);
+        int result = instance.obtenerNumeroCreditos();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of obtenerCostoCredito method, of class EstudiantePresencial.
+     */
+    @Test
+    public void testObtenerCostoCredito() {
+        System.out.println("obtenerCostoCredito");
+        double expResult = 1.0;
+        instance.establecerCostoCredito(1.0);
+        double result = instance.obtenerCostoCredito();
+        assertEquals(expResult, result, 1.0);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of obtenerMatriculaPresencial method, of class EstudiantePresencial.
+     */
+    @Test
+    public void testObtenerMatriculaPresencial() {
+        System.out.println("obtenerMatriculaPresencial");
+        double expResult = 0.0;
+        double result = instance.obtenerMatriculaPresencial();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+
+}
